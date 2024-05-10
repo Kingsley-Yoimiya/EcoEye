130c2e99507764fdf5e0620caa7408250160e82c
Kingsley-Yoimiya <153263432+Kingsley-Yoimiya@users.noreply.github.com>
2024-05-10 14:41:56 +0800

    Update result_display_screen.dart

--- flutter_app/lib/screens/result_display_screen.dart
+++ flutter_app/lib/screens/result_display_screen.dart
@@ -41,131 +41,153 @@ class _ResultDisplayScreenState extends State<ResultDisplayScreen> {
           if (snapshot.connectionState == ConnectionState.waiting) {
             return Center(child: CircularProgressIndicator());
           } else if (snapshot.hasError) {
-            return Text("Error: ${snapshot.error}");
+            return Center(
+              child: Text(
+                "Error: ${snapshot.error}",
+                style: TextStyle(
+                  fontFamily: 'Songti',
+                  fontSize: 18,
+                ),
+              ),
+            );
           } else {
             return SingleChildScrollView(
               child: Padding(
-                padding: EdgeInsets.all(8.0),
+                padding: const EdgeInsets.all(8.0),
                 child: Column(
                   crossAxisAlignment: CrossAxisAlignment.stretch,
                   children: <Widget>[
-                    // 记录概况小块，添加操作按钮
-                    Card(
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(12),
-                      ),
-                      child: Padding(
-                        padding: EdgeInsets.all(8.0),
-                        child: Column(
-                          children: [
-                            ListTile(
-                              title: Text(
-                                "记录 ID: ${snapshot.data?["recordId"]}",
-                                style: TextStyle(
-                                  fontFamily: 'Songti',
-                                  fontWeight: FontWeight.bold,
-                                ),
-                              ),
-                              subtitle: Text(
-                                "时间戳: ${snapshot.data?["timestamp"]}",
-                                style: TextStyle(fontFamily: 'Songti'),
-                              ),
-                            ),
-                            Row(
-                              mainAxisAlignment: MainAxisAlignment.end,
-                              children: <Widget>[
-                                IconButton(
-                                  icon: Icon(Icons.refresh),
-                                  onPressed: () async {
-                                    var result = await _historyController
-                                        .reanalyzeRecord(widget.recordId);
-                                    if (result["status"] == "Success") {
-                                      // 可以选择重新加载记录详情或者直接更新界面
-                                      setState(() {
-                                        recordDetail = Future.value(result);
-                                      });
-                                      ScaffoldMessenger.of(context)
-                                          .showSnackBar(
-                                        SnackBar(content: Text("重新分析成功")),
-                                      );
-                                    } else {
-                                      ScaffoldMessenger.of(context)
-                                          .showSnackBar(
-                                        SnackBar(
-                                            content: Text(result["status"])),
-                                      );
-                                    }
-                                  },
-                                ),
-                                IconButton(
-                                  icon: Icon(Icons.delete),
-                                  onPressed: () async {
-                                    String message = await _historyController
-                                        .deleteRecord(widget.recordId);
-                                    ScaffoldMessenger.of(context).showSnackBar(
-                                      SnackBar(content: Text(message)),
-                                    );
-                                    if (message ==
-                                        "Record deleted successfully") {
-                                      // 删除成功后，返回到上一个界面或者主界面
-                                      Navigator.pop(context);
-                                    }
-                                  },
-                                ),
-                                IconButton(
-                                  icon: Icon(Icons.lightbulb_outline),
-                                  onPressed: () {
-                                    // 使用 Navigator 导向 AdviceDisplayScreen，并传递 recordId
-                                    Navigator.push(
-                                      context,
-                                      MaterialPageRoute(
-                                        builder: (context) =>
-                                            AdviceDisplayScreen(
-                                                recordId: widget.recordId),
-                                      ),
-                                    );
-                                  },
-                                ),
-                              ],
-                            ),
-                          ],
-                        ),
-                      ),
-                    ),
+                    _buildRecordOverviewCard(snapshot.data),
                     SizedBox(height: 8),
-                    // 分析结果小块
-                    Card(
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(12),
-                      ),
-                      child: Padding(
-                        padding: EdgeInsets.all(8.0),
-                        child: Text(
-                          "分析结果: ${snapshot.data?["analysisResults"]}",
-                          style: TextStyle(fontFamily: 'Songti'),
-                        ),
-                      ),
-                    ),
+                    _buildAnalysisResultCard(snapshot.data?["analysisResults"]),
                     SizedBox(height: 8),
-                    // 图片小块
-                    snapshot.data?["photo"] != null
-                        ? Card(
-                            shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(12),
-                            ),
-                            child: ClipRRect(
-                              borderRadius: BorderRadius.circular(12),
-                              child: Image.network(snapshot.data!["photo"],
-                                  fit: BoxFit.cover),
-                            ),
-                          )
-                        : SizedBox(),
+                    _buildPhotoCard(snapshot.data?["photo"]),
                   ],
                 ),
               ),
             );
           }
         },
+      ),
+    );
+  }
+
+  Widget _buildRecordOverviewCard(Map<String, dynamic>? data) {
+    return Card(
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(12),
+      ),
+      child: Padding(
+        padding: EdgeInsets.all(8.0),
+        child: Column(
+          children: [
+            ListTile(
+              title: Text(
+                "ID: #${data?["recordId"]}",
+                style: TextStyle(
+                  fontFamily: 'Songti',
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              subtitle: Text(
+                "时间: ${data?["timestamp"]}",
+                style: TextStyle(
+                  fontFamily: 'Songti',
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+            _buildActionButtons(),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildActionButtons() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.end,
+      children: <Widget>[
+        IconButton(
+          icon: Icon(Icons.refresh, color: Colors.blue),
+          onPressed: () => _reanalyzeRecord(),
+        ),
+        IconButton(
+          icon: Icon(Icons.delete, color: Colors.red),
+          onPressed: () => _deleteRecord(),
+        ),
+        IconButton(
+          icon: Icon(Icons.lightbulb_outline, color: Colors.amber),
+          onPressed: () => _navigateToAdviceScreen(),
+        ),
+      ],
+    );
+  }
+
+  Widget _buildAnalysisResultCard(String? analysisResults) {
+    return Card(
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(12),
+      ),
+      child: Padding(
+        padding: EdgeInsets.all(8.0),
+        child: Text(
+          "分析结果: \n $analysisResults",
+          style: TextStyle(
+            fontFamily: 'Songti',
+            fontWeight: FontWeight.bold,
+            fontSize: 18,
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildPhotoCard(String? photoUrl) {
+    if (photoUrl == null) return SizedBox();
+    return Card(
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(12),
+      ),
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(12),
+        child: Image.network(photoUrl, fit: BoxFit.cover),
+      ),
+    );
+  }
+
+  void _reanalyzeRecord() async {
+    var result = await _historyController.reanalyzeRecord(widget.recordId);
+    if (result["status"] == "Success") {
+      setState(() {
+        recordDetail = Future.value(result);
+      });
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text("重新分析成功")),
+      );
+    } else {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text(result["status"])),
+      );
+    }
+  }
+
+  void _deleteRecord() async {
+    String message = await _historyController.deleteRecord(widget.recordId);
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(content: Text(message)),
+    );
+    if (message == "Record deleted successfully") {
+      // 删除成功后，返回到上一个界面或者主界面
+      Navigator.pop(context);
+    }
+  }
+
+  void _navigateToAdviceScreen() {
+    Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (context) => AdviceDisplayScreen(recordId: widget.recordId),
       ),
     );
   }
